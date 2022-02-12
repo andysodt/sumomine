e51e8bdeedd5c88846a28334b88b219dd31a2c09
Andy Sodt <andy@lushy.com>
2022-02-12 12:58:57 -0800

    ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,25 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer v-model="drawer" clipped app>
-      <v-list>
-        <v-list-item v-for="item in items" :key="item.text" link>
-          <v-list-item-content>
-            <v-list-item-title>
-              {{ item.text }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar color="white" dense app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title class="mr-1 align-center">
-        <span class="title">Sumomine</span>
+        <nuxt-link class="title" :to="`/`">Sumomine</nuxt-link>
       </v-toolbar-title>
       <v-spacer />
       <v-layout row align-center>
+        <nuxt-link  :to="`/news`">Live</nuxt-link>
+        <v-spacer />
+        <nuxt-link  :to="`/news`">Basho</nuxt-link>
+        <v-spacer />
+        <nuxt-link :to="`/kimarite`">Kimarite</nuxt-link>
+        <v-spacer />
+        <nuxt-link :to="`/rikishi`">Rikishi</nuxt-link>
+        <v-spacer />
+        <nuxt-link :to="`/query`">Query</nuxt-link>
+        <v-spacer />        
+        <nuxt-link :to="`/about`">About</nuxt-link>
+        <v-spacer />
         <v-text-field
+          align-center
           placeholder="Search..."
           single-line
           append-icon="mdi-magnify"
@@ -27,29 +28,7 @@
       </v-layout>
     </v-app-bar>
     <v-main>
-      <Nuxt/>
+      <Nuxt />
     </v-main>
   </v-app>
 </template>
-
-<style scoped>
-</style>
-
-<script>
-export default {
-  head: {
-    title: 'Sumomine'
-  },
-  data () {
-    return {
-      drawer: true,
-      items: [
-        { text: 'News', link: 'kimarite'  },
-        { text: 'Banzuke', link: 'kimarite'  },
-        { text: 'Kimarite', link: 'kimarite' },
-        { text: 'Rikishi', link: 'rikishi'  }
-      ]
-    }
-  }
-}
-</script>

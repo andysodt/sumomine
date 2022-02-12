13fd41386acd4ebb4360ea9db842d023f31a5495
Andy Sodt <andy@lushy.com>
2022-02-12 13:09:23 -0800

    wss

--- apollo/clientConfig.js
+++ apollo/clientConfig.js
@@ -6,6 +6,6 @@ export default function(context){
     		credentials: 'same-origin'
   		},
   		cache: new InMemoryCache(),
-	    wsEndpoint: 'ws://198.199.93.139/v1/graphql',
+	    wsEndpoint: 'wss://198.199.93.139/v1/graphql',
   	}
 }
